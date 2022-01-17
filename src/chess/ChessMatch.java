fad56cf37c9b2747f25addca876fbf00f7796bbb
Yuri Rampazo <ymrampazo@gmail.com>
2022-01-17 18:41:48 -0300

    Queen created.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 import java.util.ArrayList;
@@ -196,6 +197,7 @@ public class ChessMatch {
     placeNewPiece('a', 1, new Rook(board, Color.WHITE));
     placeNewPiece('b', 1, new Knight(board, Color.WHITE));
     placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+    placeNewPiece('d', 1, new Queen(board, Color.WHITE));
     placeNewPiece('e', 1, new King(board, Color.WHITE));
     placeNewPiece('h', 1, new Rook(board, Color.WHITE));
     placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
@@ -211,6 +213,7 @@ public class ChessMatch {
 
     placeNewPiece('a', 8, new Rook(board, Color.BLACK));
     placeNewPiece('e', 8, new King(board, Color.BLACK));
+    placeNewPiece('d', 8, new Queen(board, Color.BLACK));
     placeNewPiece('h', 8, new Rook(board, Color.BLACK));
     placeNewPiece('b', 8, new Knight(board, Color.BLACK));
     placeNewPiece('g', 8, new Knight(board, Color.BLACK));
